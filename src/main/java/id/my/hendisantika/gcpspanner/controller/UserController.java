11679c688c93c48a0728966b792093ee97d84b48
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 07:15:43 +0700

    feat: Add getAllUsers in UserController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/gcpspanner/controller/UserController.java
+++ src/main/java/id/my/hendisantika/gcpspanner/controller/UserController.java
@@ -1,9 +1,14 @@
 package id.my.hendisantika.gcpspanner.controller;
 
+import com.google.common.collect.Lists;
+import id.my.hendisantika.gcpspanner.entity.User;
 import id.my.hendisantika.gcpspanner.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    // get all users rest API
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return Lists.newArrayList(userRepository.findAll());
+    }
 }

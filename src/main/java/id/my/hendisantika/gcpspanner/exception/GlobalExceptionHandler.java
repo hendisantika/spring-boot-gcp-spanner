6b3788faf2485e020f4feaaaf820edc368aa0591
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 07:14:29 +0700

    feat: Add GlobalExceptionHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpspanner/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.gcpspanner.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-gcp-spanner
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/25
+ * Time: 07.14
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorResponse>
+    globalExceptionHandler(Exception ex, WebRequest request) {
+        CustomErrorResponse errors = new CustomErrorResponse();
+        errors.setTimestamp(LocalDateTime.now());
+        errors.setError(ex.getMessage());
+        errors.setStatus(HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(errors, HttpStatus.NOT_FOUND);
+    }
+}

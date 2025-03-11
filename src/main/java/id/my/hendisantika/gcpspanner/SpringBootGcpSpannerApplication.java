fad3a5537ae80e0fea739556a4214ebfb50ccb04
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 07:09:30 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpspanner/SpringBootGcpSpannerApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpspanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGcpSpannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGcpSpannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/gcpspanner/SpringBootGcpSpannerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.gcpspanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGcpSpannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

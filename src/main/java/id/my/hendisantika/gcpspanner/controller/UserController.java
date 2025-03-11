de05d6cdfffab199c7de915a25a0b35322a41d20
hendisantika <hendisantika@yahoo.co.id>
2025-03-11 07:15:04 +0700

    feat: Add UserController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/gcpspanner/controller/UserController.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.gcpspanner.controller;
+
+import id.my.hendisantika.gcpspanner.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-gcp-spanner
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 11/03/25
+ * Time: 07.14
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+}
